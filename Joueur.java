48de58fd86111c682743bb8496adf2ae669d4c31
HUANG Hervé <huang.herve@yahoo.fr>
2018-11-03 15:30:55 +0100

    Class abstraite avec argumentc/constructeur/2fonction

--- Joueur.java
+++ Joueur.java
@@ -1,8 +1,17 @@
 
-public interface Joueur {
-	public void jeuEnMain(); 
+public abstract class Joueur {
+	protected final int age;
+	protected final String name;
+	public Joueur(int age , String name) {
+		this.age=age;
+		this.name=name;
+	}
 	
-	public String toString();
+	public abstract void jeuEnMain(); 
+	
+	public String toString() {
+		return "Nom : "+this.name+" âge : "+this.age;
+	}
 	
 	
 	
