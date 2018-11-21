72a534e278ae0f321769d6caa2bb6b5b8c2275d1
HUANG Hervé <huang.herve@yahoo.fr>
2018-11-21 15:19:09 +0100

    Update Domino.java

--- Domino.java
+++ Domino.java
@@ -1,18 +1,25 @@
 
 public class Domino {
-
-	String haut;
-	String bas;
-
-	public Domino(String h, String b) {
-		haut = h;
-		bas = b;
-	}
-
+	private String gauche;
+	private String droit;
+	private boolean collerd;
+	private boolean collerg;
 	
+	public Domino(String g, String d){
+		  this.gauche = g;
+		  this.droit = d;
+		  this.collerd=false;
+		  this.collerg=false;
+	  }
 	
-	public String toString() {
-		
-		return "Haut: "+haut+" Bas:"+bas;
-	}
+	  public String toString() {
+		  return "Domino :"+this.gauche+"/"+this.droit;
+	  }
+	  public int getDroit() {
+		  return Integer.parseInt(droit);
+	  }
+	  public int getGauche() {
+		  return Integer.parseInt(gauche);
+	  }
+
 }
